b34d75da1fac378f54be7327e8b23c7a3e05eb52
nelsontky <nelson@ccb.wtf>
2020-11-13 23:30:00 +0800

    Fix babel issues and other bugs

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// Format: https://api.github.com//repos/{owner}/{repo}/issues/
+// Format: https://api.github.com/repos/{owner}/{repo}/issues/
 const GITHUB_ISSUES_LINK =
   "https://api.github.com/repos/nelsontky/gh-pages-url-shortener-db/issues/";
 
-const PATH_SEGMENTS_TO_SKIP = 1;
+const PATH_SEGMENTS_TO_SKIP = 0;
--- src/redirect.js
+++ src/redirect.js
@@ -1,7 +1,6 @@
 async function redirect() {
   try {
     const location = window.location;
-
     const issueNumber = location.pathname.split("/")[PATH_SEGMENTS_TO_SKIP + 1];
 
     const homepage =
@@ -12,10 +11,14 @@ async function redirect() {
       "/" +
       location.pathname.split("/")[PATH_SEGMENTS_TO_SKIP];
 
-    const response = await (
-      await fetch(GITHUB_ISSUES_LINK + issueNumber)
-    ).json();
-    const { message, title } = response;
+    const response = await fetch(GITHUB_ISSUES_LINK + issueNumber);
+
+    if (response.status !== 200) {
+      location.replace(homepage);
+    }
+
+    const payload = await response.json();
+    const { message, title } = payload;
 
     if (message === "Not Found") {
       // issueNumber does not exist in gh issues
