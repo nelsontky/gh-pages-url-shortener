e775e482a67804669522a6a7b76b91a4b9a7271f
Ole-Kenneth <ok@rait.no>
2020-11-16 16:23:23 +0100

    Make redirect thenable

--- src/redirect.js
+++ src/redirect.js
@@ -1,4 +1,4 @@
-(async function redirect() {
+(function() {
   const location = window.location;
   const issueNumber = location.pathname.split("/")[PATH_SEGMENTS_TO_SKIP + 1];
 
@@ -10,33 +10,33 @@
     "/" +
     location.pathname.split("/")[PATH_SEGMENTS_TO_SKIP];
 
-  try {
-    const response = await fetch(GITHUB_ISSUES_LINK + issueNumber);
+    fetch(GITHUB_ISSUES_LINK + issueNumber)
+      .then((result) => {
+        if (!response.ok) {
+          throw new Error('issueNumber does not exist in gh issues or other error');
+        }
 
-    if (!response.ok) {
-      throw new Error('issueNumber does not exist in gh issues or other error');
-    }
+        return response.json().then((payload) => {
+            let { message, title } = payload;
 
-    const payload = await response.json();
-    let { message, title } = payload;
+            if ((message !== "Not Found") && title) {
+              // Check if the title of issue is a legitimate URL
+              const url = new URL(title);
 
-    if ((message !== "Not Found") && title) {
-      // Check if the title of issue is a legitimate URL
-      const url = new URL(title);
-
-      if (
-        url.protocol !== "https:" ||
-        url.protocol !== "http:" ||
-        url.host === HOST
-      ) {
-        // Prevent recursive redirects and XSS
-        throw new Error('XSS');
-      } else {
-        location.replace(title);
-      }
-    }
-    throw new Error('issueNumber does not exist in gh issues');
-  } catch (e) {
-    location.replace(homepage);
-  }
+              if (
+                url.protocol !== "https:" ||
+                url.protocol !== "http:" ||
+                url.host === HOST
+              ) {
+                // Prevent recursive redirects and XSS
+                throw new Error('XSS');
+              } else {
+                location.replace(title);
+              }
+            }
+            throw new Error('issueNumber does not exist in gh issues');
+        });
+    }).catch((e) => {
+      location.replace(homepage);
+    })
 })();
