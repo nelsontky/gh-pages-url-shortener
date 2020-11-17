d873eb9b595252ee5bd268c7ba412452bdd41a2c
Joris Berthelot <joris.berthelot@careerbuilder.com>
2020-11-17 23:29:52 +0100

    Better URL validator

--- src/redirect.js
+++ src/redirect.js
@@ -1,6 +1,6 @@
 function isUrl(url) {
-  // Regex from https://stackoverflow.com/a/3809435
-  return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(
+  // Regex from https://regex101.com/r/wM8zuj/1
+  return /\bhttp(?:s)?(?::|;)\/\/(?!.+\.\.)(?'domain'.+?\..+?(?=\/|$| )).*?(?=$| )/.test(
     url
   );
 }
