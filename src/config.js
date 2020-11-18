7dc8fc9ed7cba3c5285359c506b0419ab5731e9d
nelsontky <nelson@ccb.wtf>
2020-11-18 12:22:49 +0800

    Remove dependence on babel

--- src/config.js
+++
@@ -1,7 +0,0 @@
-// Format: https://api.github.com/repos/{owner}/{repo}/issues/
-const GITHUB_ISSUES_LINK =
-  "https://api.github.com/repos/nelsontky/gh-pages-url-shortener-db/issues/";
-
-const PATH_SEGMENTS_TO_SKIP = 0;
-
-const HOST = "nlsn.cf";
